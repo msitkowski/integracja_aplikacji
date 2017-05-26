b48c80a1c305996b7e8cb82691524e2667fb2814
msitkowski <mateuszsitek.sitkowski@gmail.com>
2017-05-26 22:02:09 +0200

    client app in c#

---
+++ RecruitmentAppClient/RecruitmentAppClient/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace RecruitmentAppClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.add_new_label = new System.Windows.Forms.Label();
+            this.applicant_name_label = new System.Windows.Forms.Label();
+            this.applicant_phone_label = new System.Windows.Forms.Label();
+            this.applicant_email_label = new System.Windows.Forms.Label();
+            this.applicantNameTextBox = new System.Windows.Forms.TextBox();
+            this.applicantPhoneTextBox = new System.Windows.Forms.TextBox();
+            this.applicantEmailTextBox = new System.Windows.Forms.TextBox();
+            this.date_label = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.dataProcessingAgreementCheckbox = new System.Windows.Forms.CheckBox();
+            this.recruiterComboBox = new System.Windows.Forms.ComboBox();
+            this.recruiter_label = new System.Windows.Forms.Label();
+            this.job_label = new System.Windows.Forms.Label();
+            this.status_label = new System.Windows.Forms.Label();
+            this.jobOfferComboBox = new System.Windows.Forms.ComboBox();
+            this.statusComboBox = new System.Windows.Forms.ComboBox();
+            this.search_application_label = new System.Windows.Forms.Label();
+            this.searchMethodComboBox = new System.Windows.Forms.ComboBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.addApplicationButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.resultsDataGridView = new System.Windows.Forms.DataGridView();
+            this.notesRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.notes_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.resultsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // add_new_label
+            // 
+            this.add_new_label.AutoSize = true;
+            this.add_new_label.Location = new System.Drawing.Point(4, 12);
+            this.add_new_label.Name = "add_new_label";
+            this.add_new_label.Size = new System.Drawing.Size(104, 13);
+            this.add_new_label.TabIndex = 0;
+            this.add_new_label.Text = "Add new Application";
+            // 
+            // applicant_name_label
+            // 
+            this.applicant_name_label.AutoSize = true;
+            this.applicant_name_label.Location = new System.Drawing.Point(16, 38);
+            this.applicant_name_label.Name = "applicant_name_label";
+            this.applicant_name_label.Size = new System.Drawing.Size(82, 13);
+            this.applicant_name_label.TabIndex = 1;
+            this.applicant_name_label.Text = "Applicant Name";
+            // 
+            // applicant_phone_label
+            // 
+            this.applicant_phone_label.AutoSize = true;
+            this.applicant_phone_label.Location = new System.Drawing.Point(16, 63);
+            this.applicant_phone_label.Name = "applicant_phone_label";
+            this.applicant_phone_label.Size = new System.Drawing.Size(85, 13);
+            this.applicant_phone_label.TabIndex = 2;
+            this.applicant_phone_label.Text = "Applicant Phone";
+            // 
+            // applicant_email_label
+            // 
+            this.applicant_email_label.AutoSize = true;
+            this.applicant_email_label.Location = new System.Drawing.Point(16, 88);
+            this.applicant_email_label.Name = "applicant_email_label";
+            this.applicant_email_label.Size = new System.Drawing.Size(79, 13);
+            this.applicant_email_label.TabIndex = 3;
+            this.applicant_email_label.Text = "Applicant Email";
+            // 
+            // applicantNameTextBox
+            // 
+            this.applicantNameTextBox.Location = new System.Drawing.Point(105, 35);
+            this.applicantNameTextBox.Name = "applicantNameTextBox";
+            this.applicantNameTextBox.Size = new System.Drawing.Size(147, 20);
+            this.applicantNameTextBox.TabIndex = 4;
+            // 
+            // applicantPhoneTextBox
+            // 
+            this.applicantPhoneTextBox.Location = new System.Drawing.Point(105, 59);
+            this.applicantPhoneTextBox.Name = "applicantPhoneTextBox";
+            this.applicantPhoneTextBox.Size = new System.Drawing.Size(147, 20);
+            this.applicantPhoneTextBox.TabIndex = 5;
+            // 
+            // applicantEmailTextBox
+            // 
+            this.applicantEmailTextBox.Location = new System.Drawing.Point(105, 83);
+            this.applicantEmailTextBox.Name = "applicantEmailTextBox";
+            this.applicantEmailTextBox.Size = new System.Drawing.Size(147, 20);
+            this.applicantEmailTextBox.TabIndex = 6;
+            // 
+            // date_label
+            // 
+            this.date_label.AutoSize = true;
+            this.date_label.Location = new System.Drawing.Point(16, 116);
+            this.date_label.Name = "date_label";
+            this.date_label.Size = new System.Drawing.Size(30, 13);
+            this.date_label.TabIndex = 7;
+            this.date_label.Text = "Date";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker.Location = new System.Drawing.Point(105, 108);
+            this.dateTimePicker.MinDate = new System.DateTime(2017, 5, 26, 0, 0, 0, 0);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(147, 20);
+            this.dateTimePicker.TabIndex = 9;
+            this.dateTimePicker.Value = new System.DateTime(2017, 5, 26, 0, 0, 0, 0);
+            // 
+            // dataProcessingAgreementCheckbox
+            // 
+            this.dataProcessingAgreementCheckbox.AutoSize = true;
+            this.dataProcessingAgreementCheckbox.Location = new System.Drawing.Point(105, 134);
+            this.dataProcessingAgreementCheckbox.Name = "dataProcessingAgreementCheckbox";
+            this.dataProcessingAgreementCheckbox.Size = new System.Drawing.Size(156, 17);
+            this.dataProcessingAgreementCheckbox.TabIndex = 10;
+            this.dataProcessingAgreementCheckbox.Text = "Data processing agreement";
+            this.dataProcessingAgreementCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // recruiterComboBox
+            // 
+            this.recruiterComboBox.FormattingEnabled = true;
+            this.recruiterComboBox.Items.AddRange(new object[] {
+            "Jan Kowalski",
+            "Stefan Niedźwiedzki",
+            "Anna Straniewska",
+            "Katarzyna Kwiatkowska",
+            "Andrzej Grabski",
+            "Natalia Proszkowska"});
+            this.recruiterComboBox.Location = new System.Drawing.Point(389, 35);
+            this.recruiterComboBox.Name = "recruiterComboBox";
+            this.recruiterComboBox.Size = new System.Drawing.Size(121, 21);
+            this.recruiterComboBox.TabIndex = 11;
+            // 
+            // recruiter_label
+            // 
+            this.recruiter_label.AutoSize = true;
+            this.recruiter_label.Location = new System.Drawing.Point(333, 38);
+            this.recruiter_label.Name = "recruiter_label";
+            this.recruiter_label.Size = new System.Drawing.Size(50, 13);
+            this.recruiter_label.TabIndex = 12;
+            this.recruiter_label.Text = "Recruiter";
+            // 
+            // job_label
+            // 
+            this.job_label.AutoSize = true;
+            this.job_label.Location = new System.Drawing.Point(336, 63);
+            this.job_label.Name = "job_label";
+            this.job_label.Size = new System.Drawing.Size(24, 13);
+            this.job_label.TabIndex = 13;
+            this.job_label.Text = "Job";
+            // 
+            // status_label
+            // 
+            this.status_label.AutoSize = true;
+            this.status_label.Location = new System.Drawing.Point(336, 86);
+            this.status_label.Name = "status_label";
+            this.status_label.Size = new System.Drawing.Size(37, 13);
+            this.status_label.TabIndex = 14;
+            this.status_label.Text = "Status";
+            // 
+            // jobOfferComboBox
+            // 
+            this.jobOfferComboBox.FormattingEnabled = true;
+            this.jobOfferComboBox.Items.AddRange(new object[] {
+            "C++ Deweloper",
+            "Python Deweloper",
+            "Java Deweloper",
+            "ICT",
+            "Technical Specialist",
+            "Tester Automatyczny",
+            "Tester Manualny"});
+            this.jobOfferComboBox.Location = new System.Drawing.Point(389, 60);
+            this.jobOfferComboBox.Name = "jobOfferComboBox";
+            this.jobOfferComboBox.Size = new System.Drawing.Size(121, 21);
+            this.jobOfferComboBox.TabIndex = 15;
+            // 
+            // statusComboBox
+            // 
+            this.statusComboBox.FormattingEnabled = true;
+            this.statusComboBox.Items.AddRange(new object[] {
+            "Otrzymana",
+            "Rozmowa",
+            "Propozycja Kontraktu",
+            "Kontrakt Podpisany",
+            "Odrzucona"});
+            this.statusComboBox.Location = new System.Drawing.Point(389, 85);
+            this.statusComboBox.Name = "statusComboBox";
+            this.statusComboBox.Size = new System.Drawing.Size(121, 21);
+            this.statusComboBox.TabIndex = 16;
+            // 
+            // search_application_label
+            // 
+            this.search_application_label.AutoSize = true;
+            this.search_application_label.Location = new System.Drawing.Point(5, 255);
+            this.search_application_label.Name = "search_application_label";
+            this.search_application_label.Size = new System.Drawing.Size(96, 13);
+            this.search_application_label.TabIndex = 17;
+            this.search_application_label.Text = "Search Application";
+            // 
+            // searchMethodComboBox
+            // 
+            this.searchMethodComboBox.FormattingEnabled = true;
+            this.searchMethodComboBox.Items.AddRange(new object[] {
+            "Applicant",
+            "Job",
+            "Recruiter",
+            "Status"});
+            this.searchMethodComboBox.Location = new System.Drawing.Point(8, 272);
+            this.searchMethodComboBox.Name = "searchMethodComboBox";
+            this.searchMethodComboBox.Size = new System.Drawing.Size(197, 21);
+            this.searchMethodComboBox.TabIndex = 18;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(212, 272);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(219, 20);
+            this.searchTextBox.TabIndex = 19;
+            // 
+            // addApplicationButton
+            // 
+            this.addApplicationButton.Location = new System.Drawing.Point(435, 229);
+            this.addApplicationButton.Name = "addApplicationButton";
+            this.addApplicationButton.Size = new System.Drawing.Size(75, 23);
+            this.addApplicationButton.TabIndex = 20;
+            this.addApplicationButton.Text = "Add";
+            this.addApplicationButton.UseVisualStyleBackColor = true;
+            this.addApplicationButton.Click += new System.EventHandler(this.addApplicationButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(442, 270);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 21;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // resultsDataGridView
+            // 
+            this.resultsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.resultsDataGridView.Location = new System.Drawing.Point(8, 300);
+            this.resultsDataGridView.Name = "resultsDataGridView";
+            this.resultsDataGridView.Size = new System.Drawing.Size(509, 189);
+            this.resultsDataGridView.TabIndex = 22;
+            // 
+            // notesRichTextBox
+            // 
+            this.notesRichTextBox.Location = new System.Drawing.Point(105, 157);
+            this.notesRichTextBox.Name = "notesRichTextBox";
+            this.notesRichTextBox.Size = new System.Drawing.Size(412, 66);
+            this.notesRichTextBox.TabIndex = 23;
+            this.notesRichTextBox.Text = "";
+            // 
+            // notes_label
+            // 
+            this.notes_label.AutoSize = true;
+            this.notes_label.Location = new System.Drawing.Point(19, 157);
+            this.notes_label.Name = "notes_label";
+            this.notes_label.Size = new System.Drawing.Size(35, 13);
+            this.notes_label.TabIndex = 24;
+            this.notes_label.Text = "Notes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 501);
+            this.Controls.Add(this.notes_label);
+            this.Controls.Add(this.notesRichTextBox);
+            this.Controls.Add(this.resultsDataGridView);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.addApplicationButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.searchMethodComboBox);
+            this.Controls.Add(this.search_application_label);
+            this.Controls.Add(this.statusComboBox);
+            this.Controls.Add(this.jobOfferComboBox);
+            this.Controls.Add(this.status_label);
+            this.Controls.Add(this.job_label);
+            this.Controls.Add(this.recruiter_label);
+            this.Controls.Add(this.recruiterComboBox);
+            this.Controls.Add(this.dataProcessingAgreementCheckbox);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.date_label);
+            this.Controls.Add(this.applicantEmailTextBox);
+            this.Controls.Add(this.applicantPhoneTextBox);
+            this.Controls.Add(this.applicantNameTextBox);
+            this.Controls.Add(this.applicant_email_label);
+            this.Controls.Add(this.applicant_phone_label);
+            this.Controls.Add(this.applicant_name_label);
+            this.Controls.Add(this.add_new_label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.resultsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label add_new_label;
+        private System.Windows.Forms.Label applicant_name_label;
+        private System.Windows.Forms.Label applicant_phone_label;
+        private System.Windows.Forms.Label applicant_email_label;
+        private System.Windows.Forms.TextBox applicantNameTextBox;
+        private System.Windows.Forms.TextBox applicantPhoneTextBox;
+        private System.Windows.Forms.TextBox applicantEmailTextBox;
+        private System.Windows.Forms.Label date_label;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.CheckBox dataProcessingAgreementCheckbox;
+        private System.Windows.Forms.ComboBox recruiterComboBox;
+        private System.Windows.Forms.Label recruiter_label;
+        private System.Windows.Forms.Label job_label;
+        private System.Windows.Forms.Label status_label;
+        private System.Windows.Forms.ComboBox jobOfferComboBox;
+        private System.Windows.Forms.ComboBox statusComboBox;
+        private System.Windows.Forms.Label search_application_label;
+        private System.Windows.Forms.ComboBox searchMethodComboBox;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button addApplicationButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.DataGridView resultsDataGridView;
+        private System.Windows.Forms.RichTextBox notesRichTextBox;
+        private System.Windows.Forms.Label notes_label;
+    }
+}
+
---
+++ RecruitmentAppClient/RecruitmentAppClient/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RecruitmentAppClient.RecAppServiceRef;
+
+namespace RecruitmentAppClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            ApplicationsManagerClient am = new ApplicationsManagerClient();
+
+            int method = searchMethodComboBox.SelectedIndex;
+            string s = searchTextBox.Text;
+
+            application[] applications = null;
+
+            switch (method)
+            {
+                case 0:
+                    applications = am.searchByApplicant(s);
+                    break;
+                case 1:
+                    applications = am.searchByOffer(s);
+                    break;
+                case 2:
+                    applications = am.searchByRecruiter(s);
+                    break;
+                case 3:
+                    applications = am.searchByStatus(s);
+                    break;
+            }
+            resultsDataGridView.DataSource = applications;
+        }
+
+        private void addApplicationButton_Click(object sender, EventArgs e)
+        {
+            // random for id
+            Random r = new Random();
+
+            // create applicant object
+            applicant _applicant = new applicant();
+            _applicant.name = applicantNameTextBox.Text;
+            _applicant.phone = applicantPhoneTextBox.Text;
+            _applicant.email = applicantEmailTextBox.Text;
+
+            // create jobOffer object
+            jobOffer job = new jobOffer();
+            job.contact_email = "career@company.com";
+            job.contact_phone = "892-481-142";
+            job.min_salarySpecified = true;
+            job.max_salarySpecified = true;
+
+            int jobChoice = jobOfferComboBox.SelectedIndex;
+            Console.WriteLine(jobChoice);
+            switch (jobChoice)
+            {
+                case 0:
+                    job.title = "C++ Deweloper";
+                    job.min_salary = 8000.0;
+                    job.max_salary = 12000.0;
+                    job.description = "Wymagana znajomość systemów wbudowanych i optymalizacji oprogramowania.";
+                    job.contract_type = "Umowa o pracę";
+                    break;
+                case 1:
+                    job.title = "Python Deweloper";
+                    job.min_salary = 6800.0;
+                    job.max_salary = 9500.0;
+                    job.description = "Wymagana znajomość frameworku Django";
+                    job.contract_type = "Umowa o pracę";
+                    break;
+                case 2:
+                    job.title = "Java Deweloper";
+                    job.min_salary = 7000.0;
+                    job.max_salary = 10000.0;
+                    job.description = "Wymagana znajomość Spring Framework";
+                    job.contract_type = "B2B";
+                    break;
+                case 3:
+                    job.title = "ICT";
+                    job.min_salary = 3000.0;
+                    job.max_salary = 4500.0;
+                    job.description = "Konfiguracja sieci, serwerów Linux i Windows";
+                    job.contract_type = "Umowa o pracę";
+                    break;
+                case 4:
+                    job.title = "Technical Specialist";
+                    job.min_salary = 4000.0;
+                    job.max_salary = 6500.0;
+                    job.description = "Wymagana komunikatywność i znajomość języka niemieckiego.";
+                    job.contract_type = "B2B";
+                    break;
+                case 5:
+                    job.title = "Tester Automatyczny";
+                    job.min_salary = 5000.0;
+                    job.max_salary = 8000.0;
+                    job.description = "Wymagana wiedza w zakresie programowania w języku python.";
+                    job.contract_type = "Umowa o pracę";
+                    break;
+                case 6:
+                    job.title = "Tester Manualny";
+                    job.min_salary = 3500.0;
+                    job.max_salary = 5000.0;
+                    job.description = "Testy manualne";
+                    job.contract_type = "B2B";
+                    break;
+            }
+
+            // create application object
+            application a = new application();
+            a.id = r.Next().ToString();
+            a.applicant = _applicant;
+            a.jobOffer = job;
+            a.recruiter = recruiterComboBox.SelectedItem.ToString();
+            a.status = statusComboBox.SelectedItem.ToString();
+            a.data_processing_agreementSpecified = true;
+            a.data_processing_agreement = dataProcessingAgreementCheckbox.Checked;
+            a.date = dateTimePicker.Value.Date.ToString("dd/MM/yyyy").Replace('-', '/');
+            Console.WriteLine(dateTimePicker.Value.Date.ToString("dd/MM/yyyy"));
+            a.notes = notesRichTextBox.Text;
+
+            ApplicationsManagerClient am = new ApplicationsManagerClient();
+            am.addApplication(a);
+        }
+    }
+}
